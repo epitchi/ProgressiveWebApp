e869a1e50e49e1e272cf6ee634f036a10010d430
epitchi <thienvanlea1@gmail.com>
2021-04-04 20:33:02 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:progressive_web_app/CalendarSpace/src/CalendarSection.dart';
+import 'package:progressive_web_app/Dashboard/Dashboard.dart';
 import 'package:progressive_web_app/NavigationBar/NavigationBar.dart';
+
+import 'CalendarSpace/CalendarSpace.dart';
 
 void main() {
   runApp(MaterialApp(home: MyApp()));
@@ -9,15 +13,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Stack(
-          children: [
-            NavigationBar(),
-          ],
-        ),
-      )
-    );
+        body: Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
+        children: [
+          NavigationBar(),
+          Dashboard(),
+          CalenderSpace(),
+          ClipRRect(
+            child: Image.asset(
+              'assets/image.png',
+              height: 300.0,
+              width: 400.0,
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
